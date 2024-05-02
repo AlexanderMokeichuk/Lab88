801e0f2be879c1aab2925f2d22e87827970b1973
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-05-02 22:14:54 +0600

    Added multer.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,7 @@ import cors = require("cors");
 import express from "express";
 import mongoose from "mongoose";
 import config from "./config";
+import usersRouter from "./routers/users";
 
 
 const app = express();
@@ -12,6 +13,8 @@ const localhost = `http://localhost:${port}`;
 app.use(cors());
 app.use(express.json());
 app.use(express.static("./src/public"));
+
+app.use("/users", usersRouter);
 
 const run = async () => {
   await mongoose.connect(config.mongoose.db);
